0de9eac222c48aa6c9abaf34b877da7900737354
hexlp <hexlp@users.noreply.github.com>
2023-06-05 19:05:22 +0200

    Adding Design Pattern Examples

--- Structural/CompositeEntity/CompositeEntityExample0.cpp
+++ Structural/CompositeEntity/CompositeEntityExample0.cpp
@@ -1,28 +1,46 @@
-The Composite Entity pattern is a design pattern that aims to provide a higher-level abstraction by treating a group of related entities as a single entity. It combines multiple entities into a composite entity, which can be manipulated and managed as a whole.
+/*
+
+The Composite Entity pattern is a design pattern that aims to provide a 
+higher-level abstraction by treating a group of related entities as a 
+single entity. It combines multiple entities into a composite entity, 
+which can be manipulated and managed as a whole.
 
 The key components of the Composite Entity pattern are:
 
-1. Composite Entity: This represents the composite entity that encapsulates a group of related entities. It provides a higher-level interface for accessing and managing the underlying entities.
+1. Composite Entity: This represents the composite entity 
+   that encapsulates a group of related entities. 
+   It provides a higher-level interface for accessing and managing the underlying entities.
 
-2. Coarse-Grained Object: This is the main object that interacts with the client and represents the composite entity. It is responsible for handling requests and delegating them to the appropriate underlying entities.
+2. Coarse-Grained Object: This is the main object that 
+   interacts with the client and represents the composite entity. 
+   It is responsible for handling requests and delegating them to 
+   the appropriate underlying entities.
 
-3. Fine-Grained Objects: These are the individual entities or components that make up the composite entity. They are managed and coordinated by the composite entity.
+3. Fine-Grained Objects: These are the individual entities or 
+   components that make up the composite entity. 
+   They are managed and coordinated by the composite entity.
 
-4. Client: This is the external entity that interacts with the composite entity by making requests and receiving responses.
+4. Client: This is the external entity that interacts 
+   with the composite entity by making requests and receiving responses.
 
 The Composite Entity pattern provides several benefits:
 
-1. Abstraction: It hides the complexity of managing multiple entities behind a single composite entity, providing a simplified interface to the client.
+1. Abstraction: It hides the complexity of managing multiple 
+   entities behind a single composite entity, providing a 
+   simplified interface to the client.
+   
+2. Modularity: It allows individual entities to be modified 
+   or replaced without affecting the overall behavior of the composite entity.
 
-2. Modularity: It allows individual entities to be modified or replaced without affecting the overall behavior of the composite entity.
+3. Performance: It can optimize operations by performing bulk 
+   operations on the group of entities instead of handling them individually.
+   
+4. Encapsulation: It encapsulates the interaction and coordination 
+   between entities, providing a cohesive and unified view to the client.
+   
+*/
 
-3. Performance: It can optimize operations by performing bulk operations on the group of entities instead of handling them individually.
 
-4. Encapsulation: It encapsulates the interaction and coordination between entities, providing a cohesive and unified view to the client.
-
-Here's a simplified example to illustrate the Composite Entity pattern:
-
-```cpp
 #include <iostream>
 #include <string>
 #include <vector>
@@ -67,10 +85,21 @@ int main() {
 
     return 0;
 }
-```
 
-In this example, we have two fine-grained objects (`EntityA` and `EntityB`) that represent individual entities. The `CompositeEntity` class encapsulates these entities and provides a higher-level interface to the client. The `CompositeEntity` class delegates the processing to the underlying fine-grained objects.
+/*
 
-When the client invokes the `process` method on the `CompositeEntity` instance, it triggers the processing of both `EntityA` and `EntityB` objects as a single entity. The client doesn't need to interact with the individual entities directly; it treats the composite entity as a cohesive unit.
+In this example, we have two fine-grained objects (`EntityA` and `EntityB`) that 
+represent individual entities. The `CompositeEntity` class encapsulates these 
+entities and provides a higher-level interface to the client. 
+The `CompositeEntity` class delegates the processing to the underlying fine-grained objects.
 
-The Composite Entity pattern can be extended to handle more complex scenarios with additional entities and operations. It is particularly useful when dealing with groups of related entities that need to be managed and manipulated together.
+When the client invokes the `process` method on the `CompositeEntity` instance, 
+it triggers the processing of both `EntityA` and `EntityB` objects as a single entity. 
+The client doesn't need to interact with the individual entities directly; 
+it treats the composite entity as a cohesive unit.
+
+The Composite Entity pattern can be extended to handle more complex scenarios 
+with additional entities and operations. It is particularly useful when dealing 
+with groups of related entities that need to be managed and manipulated together.
+
+*/
